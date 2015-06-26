0bc4c74f5a4e53ca87a304c8a95edd9dc3b866e0
Leonardo Quixada <lquixada@gmail.com>
2015-06-26 19:19:42 -0300

    Tasks: instalando submodulos.

--- tasks/install.sh
+++ tasks/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "*** Instalando Apps" &&
+
+echo "** Instalando submodulos git... " &&
+
+# Atualiza pacotes se já estiverem instalados, instala se não existir
+git submodule update --init --quiet > /dev/null &&
+
+# Listando pacotes instalados apenas para conferência
+git submodule &&
+
+echo "** Instalando pacotes npm... " &&
+
 # Remove pacotes não usados
 npm prune > /dev/null &&
 
