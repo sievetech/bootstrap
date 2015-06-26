758153c206b4427c825bf7cd945bdca849decae2
Leonardo Quixada <lquixada@gmail.com>
2015-06-26 16:54:02 -0300

    primeiro commit.

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/static/collect/style/"
+http_images_dir = "images"
+http_fonts_dir = "fonts"
+http_css_dir = "sheets"
+
+project_path = "style"
+images_dir = "images"
+fonts_dir = "fonts"
+css_dir = "sheets"
+
+relative_assets = false
