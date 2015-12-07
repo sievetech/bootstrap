c542aa9c2052d1fb18d9fe7a50d6278172f69d21
Leonardo Quixada <lquixada@gmail.com>
2015-12-07 13:23:51 -0200

    Ajustando css do tema do bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
               {
                   expand: true,
                   cwd: './',
-                  src: ['src/**/*.less', 'demo/**/*.less'],
+                  src: ['src/**/*.less', 'demo/**/*.less', 'theme/**/*.less'],
                   dest: './',
                   ext: '.css'
               }
