76ee69c50bee9de3c66af7e4202e5605eaf4a1dc
Leonardo Quixada <lquixada@gmail.com>
2016-04-14 11:55:20 -0300

    Varíaveis de ambiente para o SASS removidas.

--- tasks/ci.sh
+++ tasks/ci.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Setando variáveis senão dá erro de encoding no sass
-export LC_ALL="en_US.UTF-8"
-export LANG="en_US.UTF-8"
-
 # Preparando o ambiente
 source ./tasks/clean.sh &&
 source ./tasks/install.sh &&
